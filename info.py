d57c389a3efae2c04ed6cd17e80f280fcb9cb920
Nkbots001 <nkstores@protonmail.com>
2024-11-25 17:08:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '24980957'))
+API_HASH = environ.get('API_HASH', 'eb547148c7c563c66afe0ebad929ad40')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7637667505:AAEV3Jve21aXLngVmemDzJRHzsp2sOy8cII")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
@@ -20,14 +20,14 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://file-to-link-r520.onrender.com")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002300036761'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Nkbot001:nkbot123@cluster0.5crfy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Nkbot001")
 
 # Shortlink Info
